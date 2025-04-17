34b3f2bb90627ad2244637d6079c2c125c757867
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2025-04-17 17:13:53 -0300

    feat: add Fastify compression and helmet for enhanced security and performance

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,29 @@ import {
   type NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 
+import compression from '@fastify/compress';
+import helmet from '@fastify/helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({
+      trustProxy: true,
+      logger: true,
+      ignoreTrailingSlash: true,
+    }),
   );
+
+  await app.register(helmet, {
+    hidePoweredBy: true,
+  });
+  await app.register(compression);
+
+  app.setGlobalPrefix('/api', {
+    exclude: ['/', '/health'],
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
