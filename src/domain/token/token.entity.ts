6f98945eb2afcd41b3577fc779901fdf5d322e37
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2025-04-23 15:33:27 -0300

    feat: add Token entity with properties and relationships for MikroORM integration

---
+++ src/domain/token/token.entity.ts
@@ -0,0 +1,46 @@
+import { Entity, Index, ManyToOne, Property } from '@mikro-orm/core';
+
+import { BaseEntity } from '@domain/base.entity';
+import { User } from '@domain/user/user.entity';
+
+@Entity({
+  tableName: 'tokens',
+})
+export class Token extends BaseEntity {
+  @Property({
+    length: 255,
+  })
+  @Index()
+  token: string;
+
+  @Property({
+    name: 'ip_address',
+    length: 100,
+  })
+  ipAddress: string;
+
+  @Property({
+    name: 'user_agent',
+    length: 255,
+  })
+  userAgent: string;
+
+  @Property({
+    name: 'revoked_date_time',
+    nullable: true,
+  })
+  revokedDateTime?: Date;
+
+  @ManyToOne(() => User, {
+    nullable: true,
+    joinColumn: 'user_id',
+  })
+  user?: User;
+
+  @Property({
+    name: 'user_id',
+    length: 21,
+    nullable: true,
+  })
+  userId?: string;
+}
